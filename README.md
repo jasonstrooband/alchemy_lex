19672f2b206d02753e05dae96c9ccb97f9c6ff8e
Jason S <jason@jasonstrooband.com>
2020-08-10 10:03:58 +1000

    -Added README.md -Added more tokens to tokenizer.js -Changed the way the regex matches with a token on a line by not using substr but an offset so that lookbehind's can still work to some degree -JS Version has a lot of hurdles to overcome with lacking of regex features and is on hold for the time being

---
+++ README.md
@@ -0,0 +1,47 @@
+# Alchemy Lex
+
+A lexical tool to take a premade script and output some randomness based on that script
+
+## Description
+
+Alchemy Lex is a lexical analyser that takes a predefined script and pours over it to configure the data ina a way that can be evaluated for a specific and random putput. This tool is designed in a way to be lightweight and powerful. A sister project will be underway shortly with a website that fully implements this project in a user friendly way.
+
+## Getting Started
+
+### Dependencies
+
+* Must use a local (or remote) web server to run the tool e.g. wamp. This is to run the php still included (I hope to remove this need sometime soon).
+
+### Installing
+
+* Place the files inside the document root of your web server
+* No modifications should be necessary
+
+### Executing program
+
+* Access the localhost of your web server and navigate to the sub folder for this project
+* Select a script from the dropdown and hit go
+* **Alternatvely** you can go to the jsver.php page for a version that runs on javascript and is under construction to get it up to date qith the php version.
+
+>See [Features & Roadmap](https://github.com/jasonstrooband/alchemy_lex/wiki/Features-and-Roadmap) page for more information.<br>
+>Warning ^^ infodump.
+
+## Author
+
+Me, Jason S
+
+## Version History
+
+* V2a
+  * Development
+* V1a
+  * First alpha release
+  * Very basic functionality abd proof of concept
+
+## License
+
+This project is licensed under the GNU GPLv3 License
+
+## Acknowledgments
+
+This application has been heavily inspired by a nifty little tool called [Tablesmith](http://www.mythosa.net/p/tablesmith.html) by Bruce Gulke it's definately worth checking it out.
--- includes/tokenizer.js
+++ includes/tokenizer.js
@@ -1,18 +1,30 @@
 class Tokenizer {
   terminals_comments = {
-    T_LINECOMMENT:        /^#.+\r?/,
-    T_BLOCKCOMMENT_OPEN:  /^\/\*/,
-    T_BLOCKCOMMENT_CLOSE: /^\*\//
+    T_LINECOMMENT:        /#.+\r?/,
+    T_BLOCKCOMMENT_OPEN:  /\/\*/,
+    T_BLOCKCOMMENT_CLOSE: /\*\//
   };
 
   terminals_special = {
-    T_GROUP_IDENTIFIER: /^[:;|]/
+    T_GROUP_IDENTIFIER: /^[:;|]/,
+    T_GROUP_NAME: /(?<=[:;|])[a-zA-Z0-9\_\-]+/,
+    T_GROUP_OPEN_BRACKET: /\{/,
+    //T_GROUP_CLOSE_BRACKET: /\}/,
+    //T_GROUP_LINE_RANGE_NUMBER: /\d+\-\d+\:/,
+    //T_GROUP_LINE_SINGLE_NUMBER: /\d+\:/,
+    //T_GROUP_LINE_EQUAL_NUMBER: /\s+\:/,
+    //T_GROUPCALL_OPEN_BRACKET: /\[/,
+    //T_GROUPCALL_CLOSE_BRACKET: /\]/,
+    //T_FUNCTIONCALL_OPEN_BRACKET: /\</,
+    //T_FUNCTIONCALL_CLOSE_BRACKET: /\>/,
+    //T_EXPRESSION_OPEN_BRACKET: /\(/,
+    //T_EXPRESSION_CLOSE_BRACKET: /\)/
   };
   
   terminals_general = {
-    T_NEWLINE: /^\r/,
-    T_WHITESPACE: /^\s+/,
-    T_STRING: /^\b.+?(?=[\(\<\>\[\]\r\n\~])/
+    T_NEWLINE: /\r/,
+    T_WHITESPACE: /\s+/,
+    //T_STRING: /\b.+?(?=[\(\<\>\[\]\r\n\~])/
     //"/\G([\.\,\;\:\?\!\'\"\-\_\/\~])/" => "T_PUNCTUATION",
     //"/\G([+-]?[0-9]*[.][0-9]+)/"       => "T_FLOAT",
     //"/\G(\d+)/"                        => "T_NUMBER",
@@ -36,15 +48,13 @@ class Tokenizer {
     // Explode into a line array
     //input = input.match(/[^\r\n]+/g);
     input = input.split(/\n/g);
-    //console.log(JSON.stringify(input, null /*replacer function */, 4 /* space */))
+    console.log(JSON.stringify(input, null /*replacer function */, 4 /* space */))
 
     execution: {
       for (let num = 0; num < input.length; num++) {
         this.offset = 0;
         let line = input[num];
-        console.log("Line: " + (num+1) + " - " + JSON.stringify(line));
-  
-        console.log(num);
+        //console.log("Line: " + (num+1) + " - " + JSON.stringify(line));
   
         while(this.offset < line.length){
           let result = '';
@@ -55,9 +65,18 @@ class Tokenizer {
             break execution;
           }
           this.tokens.push(result);
+          console.log('Add Offset: ' + result['value'].length)
           this.offset += result['value'].length;
+          console.log('Line len: ' + line.length + ' - Offset: ' + this.offset);
         }
       }
+      
+      this.tokens.push({
+        token: 'T_EOF',
+        value: '',
+        line: input.length,
+        offset: 1
+      });
     }
 
     //console.log(JSON.stringify(input, null /*replacer function */, 4 /* space */))
@@ -65,18 +84,26 @@ class Tokenizer {
 
   match(line, num) {
     let str = line.substr(this.offset);
+    console.log(this.offset);
+
+    console.log('Match: ' + JSON.stringify(str));
 
     for (const [key, value] of Object.entries(this.terminals)) {
-      let match = str.match(value);
+      let regexp = new RegExp(value, 'g');
+      regexp.lastIndex = this.offset;
+      let match = regexp.exec(line);
+
+      // Having trouble getting it to match T_WHITESPACE before T_GROUP_OPEN_BRACKET
 
       if(match) {
+        console.log(match);
+        console.log('Matched: ' + key);
         let token = {
           token: key,
           value: match[0],
           line: num + 1,
           offset: this.offset + 1
         }
-
         return token;
       }
     }
